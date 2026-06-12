a2751ee8171e34cdc7aa2432a24ffc24a43bd63c
nknitipong-eng <nk.nitipong@gmail.com>
2026-06-12 10:50:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -131,6 +131,22 @@ else:
         (price_df["Scope"] != "Pull & Clean")
     ]
 
+# =========================
+# COST TABLE
+# =========================
+cost_df = cost_filter[["EQ", "Scope", "He_type", "Time", "Price"]].copy()
+
+cost_df.rename(columns={
+    "Price": "Unit Rate"
+}, inplace=True)
+
+# ✅ FIX 2: ให้ Qty แก้ได้
+if "Qty" not in cost_df.columns:
+    cost_df["Qty"] = 1
+
+# คำนวณ Total
+cost_df["Total"] = cost_df["Unit Cost"] * cost_df["Qty"]
+
 
 # =========================
 # EDITABLE TABLE
