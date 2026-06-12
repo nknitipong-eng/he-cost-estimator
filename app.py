96f7d13b7c4965ed97eca1736947b1f2e9b32ca9
nknitipong-eng <nk.nitipong@gmail.com>
2026-06-12 14:00:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import io
 
-st.set_page_config(page_title="Heat Exchanger Cost Estimator", layout="short")
+st.set_page_config(page_title="Heat Exchanger Cost Estimator", layout="centered")
 
 st.title("🔥 Heat Exchanger Cost Estimator")
 
